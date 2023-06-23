aef54265bc7e93d52f6888d743926ecb04b64b90
Aishwariya1991 <136902253+Aishwariya1991@users.noreply.github.com>
2023-06-23 16:22:33 -0400

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -37,8 +37,8 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("SELECT * from fruit_load_list")
-streamlit.text(my_data_row)
+streamlit.header("SELECT * from fruit_load_list")
+streamlit.dataframe(my_data_row)
 
 
 
